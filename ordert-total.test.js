455051cd061facc6873077d070b0ad6c7fb59416
giao <giao.dinh@gmail.com>
2018-02-12 22:21:55 +0100

    2nd commit

--- ordert-total.test.js
+++ ordert-total.test.js
@@ -4,49 +4,38 @@
 
 const orderTotal = require('./order-total');
 
-it('works', () => {
-	expect(1).toBe(1);
-});
-
-it('Quantity', () => {
+it('Quantity', () =>
 	expect(orderTotal({
 		items: [
 			{ name: 'Dragon candy', price: 2, quantity: 3 },
 		]
 	})).toBe(6)
-})
+)
 
-// it('Quantity specified', () => {
-// 	expect(orderTotal({
-// 		items: [
-// 			{ name: 'Dragon candy', price: 2, quantity: 3 },
-// 		]
-// 	})).toBe(3)
-// })
+it('No quantity specified', () =>
+	expect(orderTotal({
+		items: [
+			{ name: 'Dragon candy', price: 3 },
+		]
+	})).toBe(3)
+)
 
-// if (orderTotal({
-// 	items: [
-// 		{ name: 'Dragon candy', price: 3 },
-// 	]
-// }) !== 3) {
-// 	throw new Error('Check fail: no quantity specified');
-// }
+it('happy path (Example 1)', () =>
+	expect(orderTotal({
+		items: [
+			{ name: 'Dragon food', price: 8, quantity: 1 },
+			{ name: 'Dragon cage(small)', price: 800, quantity: 1 }
+		]
+	})).toBe(808)
+)
 
-if (orderTotal({
-	items: [
-		{ name: 'Dragon food', price: 8, quantity: 1 },
-		{ name: 'Dragon cage(small)', price: 800, quantity: 1 }
-	]
-}) !== 808) {
-	throw new Error('Check fail: happy path (Example 1)');
-}
+it('happy path  (Example 2)', () =>
+	expect(orderTotal({
+		items: [
+			{ name: 'Dragon collar', price: 20, quantity: 1 },
+			{ name: 'Dragon chew toy', price: 40, quantity: 1 }
+		]
+	})).toBe(60)
+)
 
-if (orderTotal({
-	items: [
-		{ name: 'Dragon collar', price: 20, quantity: 1 },
-		{ name: 'Dragon chew toy', price: 40, quantity: 1 }
-	]
-}) !== 60) {
-	throw new Error('Check fail: happy path  (Example 2)');
-}
 
